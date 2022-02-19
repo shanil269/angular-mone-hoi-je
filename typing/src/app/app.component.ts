e1d8546e24d23a3d8e473dfcec1de16a8829fd18
shanil269 <shanilhossain2609@gmail.com>
2022-02-19 19:34:38 +0600

    a typing game

---
+++ typing/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { lorem } from 'faker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(value: string) {
+    this.enteredText = value;
+  }
+
+  compare(randomChar: string, enteredChar: string) {
+    if(!enteredChar) {
+      return 'pending';
+    }
+    return randomChar === enteredChar ? 'correct' : 'incorrect';
+
+  }
+}
