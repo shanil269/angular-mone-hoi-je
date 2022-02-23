bae072bf2477e3af93d76490f4247975fd9d5663
shanil269 <shanilhossain2609@gmail.com>
2022-02-23 18:30:17 +0600

    implemented rxjs to fetch data

--- wsearch/src/app/wikipedia.service.ts
+++ wsearch/src/app/wikipedia.service.ts
@@ -1,5 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { pluck } from 'rxjs/operators';
+
+interface WikipediaResponse {
+  query: {
+    search: {
+      title: string;
+      snippet: string;
+      pageid: number;
+      wordcount: number;
+    }[];
+  };
+}
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +20,7 @@ export class WikipediaService {
   constructor(private http: HttpClient) { }
 
   search(term: string) {
-    return this.http.get('https://en.wikipedia.org/w/api.php', {
+    return this.http.get<WikipediaResponse>('https://en.wikipedia.org/w/api.php', {
       params: {
         action: 'query',
         format: 'json',
@@ -17,6 +29,6 @@ export class WikipediaService {
         srsearch: term,
         origin: '*',
       }
-    });
+    }).pipe(pluck('query', 'search'));
   }
 }
